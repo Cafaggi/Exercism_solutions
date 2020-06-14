186cf42e85c9bf5a7cd59ab4a994b07f6e2d865b
Carlos cafaggi <carlos.cafaggi@gmail.com>
2020-06-14 14:31:56 -0500

    solve error handling exercise
    note tupple error construction

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,14 +1,20 @@
 def handle_error_by_throwing_exception():
-    pass
+    raise Exception("This is an error")
 
 
 def handle_error_by_returning_none(input_data):
-    pass
+    if input_data.isnumeric():
+        return int(input_data)
+    else:
+        return None
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    tested_input_data = handle_error_by_returning_none(input_data)
+    return (tested_input_data != None, tested_input_data)
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+    with filelike_object as file:
+        filelike_object.do_something()
+
