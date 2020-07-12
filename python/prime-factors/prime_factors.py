27a4d544d611654dba2ec5e020604ed476016a56
Carlos cafaggi <carlos.cafaggi@gmail.com>
2020-07-12 17:27:42 -0500

    solves primer factors exercises

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,15 @@
-def factors(value):
-    pass
+def factors(value: int):
+
+	factor, prime_number = (list(), 2)
+	
+	if value < 1:
+		raise ValueError('value must be greater than 1')
+
+	while value >= prime_number:
+		if value%prime_number == 0:
+			factor.append(prime_number)
+			value = value/prime_number
+		else:
+			prime_number += 1
+
+	return factor
