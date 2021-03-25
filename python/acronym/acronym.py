5aea3f1edb0eac067921a9569cec55389ee4fdd9
Cafaggi <caffa1006@gmail.com>
2021-03-25 13:04:41 -0600

    allegies exercise

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,11 +1,6 @@
+import re
 
 def abbreviate(words: 'srt') -> 'str':
-
-    '''
-    get the acronym a pharse
-    '''
-
-    import re
     words = re.sub(r"[^a-zA-Z('^a-zA-Z)]+",' ', words).split()
     return "".join([word[0].upper() for word in words])
     
