9ac0a8d7bf130c206a189db1e94fb133ed92b901
cafaggi <carlos.cafaggi@gmail.com>
2021-03-29 09:31:19 -0600

    solve perfect numbers exercise

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,17 +1,13 @@
 def classify(number):
-    try:
-        ran=range(number-1,1)
-        tot=0
-        for i in ran:
-            if number % i==0:
-                tot+=i
-        if tot == number:
-            return 'perfect'
-        elif tot < number:
-            return 'deficient'
-        elif tot > number:
-            return 'abundant'
-        
-    except ValueError as e:
-        return str(e)
-    
+    if number <= 0:
+        raise ValueError('the number must be natural')
+
+    aliquote_sum = sum([factor for factor in range(1, number) if number%factor == 0])
+
+    if aliquote_sum > number:
+        return 'abundant'
+    elif aliquote_sum == number:
+        return 'perfect'
+    else :
+        return 'deficient'
+
