952a360c36caeac5e67fc7c80958458d5f5811a2
cafaggi <carlos.cafaggi@gmail.com>
2021-03-29 23:24:34 -0600

    shift operation for exponential optimization

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,6 @@
 def square(number):
     if number > 64 or number <= 0: raise ValueError('number must be grater than 0 and smaller or equal than 64')
-    return 2**(number-1)
+    return 1 << (number-1)
 
 
 def total():
