0d0374ea7b5db6edb19993b9e2357b3231deb8d8
cafaggi <carlos.cafaggi@gmail.com>
2021-03-29 23:16:35 -0600

    solve grains exercise

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,7 @@
 def square(number):
-    pass
+    if number > 64 or number <= 0: raise ValueError('number must be grater than 0 and smaller or equal than 64')
+    return 2**(number-1)
 
 
 def total():
-    pass
+    return 2**64 -1
