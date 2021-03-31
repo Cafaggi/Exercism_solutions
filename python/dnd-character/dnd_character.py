569dc9c1bd89860c1ab68c23aefcf0626b389476
cafaggi <carlos.cafaggi@gmail.com>
2021-03-30 23:06:12 -0600

    solve dnd exercise

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,3 +1,27 @@
+from dataclasses import dataclass, fields
+from numpy.random import randint
+
+
+@dataclass
 class Character:
+
+    strength: int
+    dexterity: int
+    constitution: int
+    intelligence: int
+    wisdom: int
+    charisma: int
+
     def __init__(self):
-        pass
+        for field in fields(self):
+            setattr(self, field.name, self.ability())
+        self.hitpoints =  10 + modifier(self.constitution)
+
+    def ability(self) -> int:
+        return sum(sorted(randint(1,6,size=6))[:3])
+    
+def modifier(constitution: int) -> int:
+    return (constitution-10)//2
+
+ch = Character()
+ch.strength
