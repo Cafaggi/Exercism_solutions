554ecb69e5af2a7bf5fb418d099d27f00d49d1d7
cafaggi <carlos.cafaggi@gmail.com>
2021-04-03 16:43:30 -0600

    solve allergies exercise

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,24 @@
 class Allergies:
 
     def __init__(self, score):
-        pass
+        self.score = score
+
+        self.allergies ={
+                        1: 'eggs',
+                        2: 'peanuts',
+                        4: 'shellfish',
+                        8: 'strawberries',
+                        16: 'tomatoes',
+                        32: 'chocolate',
+                        64: 'pollen',
+                        128: 'cats'
+        }
+
+        self.items = [v for k, v in self.allergies.items() if k == (score & k)]
 
     def allergic_to(self, item):
-        pass
+        return item in self.items
 
     @property
     def lst(self):
-        pass
+        return self.items
