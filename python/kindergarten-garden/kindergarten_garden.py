c821faf3728e9a8f319da2e6a5755666363d4fb9
cafaggi <carlos.cafaggi@gmail.com>
2021-04-05 18:47:34 -0500

    upgrade garden exercise

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,33 +1,46 @@
+DEFAULT_STUDENTS = ['Alice',
+                    'Bob',
+                    'Charlie',
+                    'David',
+                    'Eve',
+                    'Fred',
+                    'Ginny',
+                    'Harriet',
+                    'Ileana',
+                    'Joseph',
+                    'Kincaid',
+                    'Larry']
+
+PLANT_MAP = {'G': 'Grass', 
+            'C': 'Clover', 
+            'R':'Radishes', 
+            'V':'Violets'}
+
 
 class Garden:
 
-    def __init__(self, diagram, students = []):
+    def __init__(self, diagram, students = DEFAULT_STUDENTS):
 
-        self.plant_map = {  'G': 'Grass', 
-                            'C': 'Clover', 
-                            'R':'Radishes', 
-                            'V':'Violets'}
-
-        default_students = ['Alice',
-                            'Bob',
-                            'Charlie',
-                            'David',
-                            'Eve',
-                            'Fred',
-                            'Ginny',
-                            'Harriet',
-                            'Ileana',
-                            'Joseph',
-                            'Kincaid',
-                            'Larry']
-
-        self.students = students if students else default_students
-        self.students.sort()
-
-        self.diagram = diagram.split("\n")
-
+        self.students = students
+        self.diagram = diagram.splitlines()
+        self.students_garden = self.create_students_map()
     
     def plants(self, student):
-        index_end = (self.students.index(student)+1)*2
-        plant_list = self.diagram[0][index_end-2:index_end]+self.diagram[1][index_end-2:index_end]
-        return [self.plant_map[plant] for plant in plant_list]
+        return self.students_garden[student]
+
+    def create_students_map(self):
+
+        '''
+        Create a dictionary with all de students as keys and its garden's plants as values
+        '''
+
+        student_map = dict()
+        self.students.sort()
+
+        for student in self.students:
+            index_end = (self.students.index(student)+1)*2
+            plant_list = self.diagram[0][index_end-2:index_end]+self.diagram[1][index_end-2:index_end]
+            student_map[student] = [PLANT_MAP[plant] for plant in plant_list]
+
+        return student_map
+        
--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,12 @@
 def saddle_points(matrix):
-    pass
+    #Validate matrix
+    if any(len(row) != len(matrix[0]) for row in matrix): raise ValueError("The supplied matrix is invalid.")
+    
+    #search for saddle points
+    rows, cols = matrix, list(zip(*matrix))
+    return [ 
+            {"row": x+1, "column": y+1} 
+            for x, row in enumerate(matrix) 
+            for y, value in enumerate(row) 
+            if max(rows[x]) == min(cols[y]) 
+            ]
